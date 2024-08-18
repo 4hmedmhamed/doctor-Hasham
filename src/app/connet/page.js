e22be42a56605d0e080b51cf4416e24d6955a447
4hmedmhamed <ah5733956@gmail.com>
2024-08-18 04:18:15 +0300

    hhhhhj

--- src/app/connet/page.js
+++ src/app/connet/page.js
@@ -38,18 +38,20 @@ function page() {
           
 
 
+          <h2>Send e-mail to someone@example.com:</h2>
+
+<form action="mailto:someone@example.com" method="post" enctype="text/plain">
+Name:<br />
+<input type="text" name="name" /><br />
+E-mail:<br />
+<input type="text" name="mail" /><br />
+Comment:<br />
+<input type="text" name="comment" size="50" /><br /><br />
+<input type="submit" value="Send" />
+<input type="reset" value="Reset" />
+</form>
           </div>
 
-          <form action="mailto:someone@example.com" method="post" enctype="plain">
-            Name:<br />
-            <input type="text" name="name" /><br />
-            E-mail:<br />
-            <input type="text" name="mail" /><br />
-            Comment:<br />
-            <input type="text" name="comment" size="50" /><br /><br />
-            <input type="submit" value="Send" />
-            <input type="reset" value="Reset" />
-          </form>
         
 
           <div>
