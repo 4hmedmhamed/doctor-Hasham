014e3dbcaab729093ffc336721c156f1aa00b4d9
4hmedmhamed <ah5733956@gmail.com>
2024-08-18 04:29:56 +0300

    uoo

--- src/app/connet/page.js
+++ src/app/connet/page.js
@@ -36,10 +36,60 @@ function page() {
           <div className='border rounded'>
 
           
-
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3421.7232975658176!2d31.167332559839995!3d70.950291675191647!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14f7bb5be6b749dd%3A0xca61211e438e63e2!2z2YXYsdmD2LIg2YLYtdixINin2YTZhtmK2YQg2YTZhNmG2LPYp9ihINmI2KfZhNiq2YjZhNmK2K8!5e0!3m2!1sar!2seg!4v1722531984751!5m2!1sar!2seg" 
+                            width="100%" height="250px" 
+                             allowfullscreen="" 
+                            loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
           </div>
+<div>
+  <h2>تقدر تستفسر اوتبعت استشاره</h2>
+  
+  <div className='d-flex'>
+  <div className=' mx-2'>
+            <Link href={'https://www.facebook.com/share/USdGviMw2jHAcJ1i/?mibextid=qi2Omg'}>
+           <Image src={face} 
+           width={70}
+           height={70}
+           style={{borderRadius:"20px"}}>
 
+           </Image>
+             </Link>
+          </div>
+          <div className=' mx-2'>
+            <Link href={'https://youtu.be/m-BqR-lU8RY?si=C_EAJAEb3rBNVPfz'}>
+           <Image src={you} 
+           width={70}
+           height={70}
+           style={{borderRadius:"20px"}}>
+
+           </Image>
+             </Link>
+          </div>
+          <div className=' mx-2'>
+       
+            <Link href={'tel:01066726667'}>
+           <Image src={call} 
+           width={70}
+           height={70}
+           style={{borderRadius:"20px"}}>
+
+           </Image>
+             </Link>
+          </div>
+          <div className='6 mx-2'>
+            <Link href={'https://api.whatsapp.com/send?phone=01066726667'}>
+           <Image src={wath} 
+           width={70}
+           height={70}
+           style={{borderRadius:"20px"}}
+           >
+             
+           </Image>
+             </Link>
+          </div>
+  </div>
+</div>
         
 
           <div>
